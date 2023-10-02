a327612a8e66d2e751c8d164b7392b262f106ac1
ashulme96 <anshulme96@.com>
2023-10-02 07:38:37 +0530

    modiefied project structure

--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -7,6 +7,8 @@ import { Todo } from '../models/todo'
 
 let todos: Todo[] = [];
 
+type RequestBody={text : string};
+type RequestParams={todoId:string}
 
 const router = Router();
 router.get('/', (req, res, next) => {
@@ -14,31 +16,36 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/todo', (req, res, next) => {
+    const body=req.body as RequestBody
     const newTodo: Todo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text:  body.text,
     };
     todos.push(newTodo);
     res.status(200).json({newTodo})
 })
 
 router.put('/todo/:todoId', (req, res, next) => {
-    const tid = req.params.todoId;
+    const param=req.params as RequestParams
+    const body=req.body as RequestBody
+    const tid = param.todoId;
 
     const todoIndex = todos.findIndex(todonext => (todonext.id === tid))
 
     if (todoIndex >= 0) {
         todos[todoIndex] = {
             id: todos[todoIndex].id,
-            text: req.body.text
+            text: body.text
         }
         return res.status(200).json({ message: "updated todos", todos: todos })
     }
     res.status(401).json({ message: 'something is wrong' })
 })
 
-router.delete('/todo/:id', (req, res, next) => {
-    const id = req.params.id;
+router.delete('/todo/:todoId', (req, res, next) => {
+    const param=req.params as RequestParams
+    const id = param.todoId;
+
     if (!id) {
         return res.sendStatus(404).json({ success: false })
     } else {
